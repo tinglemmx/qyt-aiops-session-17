368bf067ffc7d912755a46efa12f32c2c9ad41c2
Tingle <tingle.mmx@hotmail.com>
2025-08-25 15:22:30 +0800

    feat(第二天作业更新): 更新fstring的写法

--- homework/Day_2/Question4.py
+++ homework/Day_2/Question4.py
@@ -10,12 +10,12 @@ COURSE_FEES_SEC = 456789.12456
 COURSE_FEES_Python = 1234.3456
 
 
-line1 = "Department1 name:%-10s Manager:%-10s COURSE FEES:%-10.2f The End!" % (
-    department1, depart1_m, COURSE_FEES_SEC
-)
-line2 = "Department2 name:%-10s Manager:%-10s COURSE FEES:%-10.2f The End!" % (
-    department2, depart2_m, COURSE_FEES_Python
-)
+# line1 = "Department1 name:%-10s Manager:%-10s COURSE FEES:%-10.2f The End!" % (
+#     department1, depart1_m, COURSE_FEES_SEC
+# )
+# line2 = "Department2 name:%-10s Manager:%-10s COURSE FEES:%-10.2f The End!" % (
+#     department2, depart2_m, COURSE_FEES_Python
+# )
 
 # line1 = "Department1 name:{:<10} Manager:{:<10} COURSE FEES:{:<10.2f} The End!".format(
 #     department1, depart1_m, COURSE_FEES_SEC
@@ -23,6 +23,20 @@ line2 = "Department2 name:%-10s Manager:%-10s COURSE FEES:%-10.2f The End!" % (
 # line2 = "Department2 name:{:<10} Manager:{:<10} COURSE FEES:{:<10.2f} The End!".format(
 #     department2, depart2_m, COURSE_FEES_Python
 # ) 
+
+line1 = (
+    f"Department1 name:{department1:<10} "
+    f"Manager:{depart1_m:<10} "
+    f"COURSE FEES:{COURSE_FEES_SEC:<10.2f} "
+    f"The End!"
+)
+
+line2 = (
+    f"Department2 name:{department2:<10} "
+    f"Manager:{depart2_m:<10} "
+    f"COURSE FEES:{COURSE_FEES_Python:<10.2f} "
+    f"The End!"
+)
 
 
 length = len(line1)
