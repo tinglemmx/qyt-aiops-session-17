bc41ae803a186f87421296d53d5e9046ed62ed70
Tingle <tingle.mmx@hotmail.com>
2025-08-25 14:37:53 +0800

    feat(第二天作业更新): 修改状态匹配的正则

--- homework/Day_2/re_ip.py
+++ homework/Day_2/re_ip.py
@@ -6,7 +6,7 @@ def get_info(string):
         r'(?P<ip>\d{1,3}(?:\.\d{1,3}){3})\s+'
         r'\S+\s+'                      # 跳过 YES/NO
         r'\S+\s+'                      # 跳过 CONFIG/STATIC
-        r'(?P<status>UP|DOWN|ADMIN DOWN)'
+        r'(?P<status>up|down|administratively down)',
     )
     match = pattern.match(string)
     if match:
@@ -14,13 +14,16 @@ def get_info(string):
     return None
 
 if __name__ == "__main__":
-    str1='Port-channel1.189     192.168.189.254 YES   CONFIG  UP'
+    str1='Port-channel1.189     192.168.189.254 YES   CONFIG   up '
     info = get_info(str1)
     fields = [
         ("接口", "interface"),
         ("IP地址", "ip"),
         ("状态", "status"),
     ]
-    print('-'*80)
-    for label, key in fields:
-        print(f'{label:<10}：{info[key]}')
+    if info:
+        print('-'*80)
+        for label, key in fields:
+            print(f'{label:<10}：{info[key]}')
+    else:
+        print('No match')
