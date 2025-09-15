7acc4a8754dd9fc67d6ceba015677027dfbeb241
Tingle <tingle.mmx@hotmail.com>
2025-09-15 14:05:10 +0000

    chore(作业更新): NetDevOps 经典自动化协议 第一天 [构建免费ARP]

--- homework/classic_network_protocols/Day_1/generate_gratuitous_arp.py
+++ homework/classic_network_protocols/Day_1/generate_gratuitous_arp.py
@@ -1,20 +1,21 @@
-from kamene.all import *  # Scapy 3.0+ Python 3.11 用 Kamene
+import logging
+from kamene.layers.l2 import Ether, ARP
+from kamene.sendrecv import sendp
 
-# 配置参数
-iface = "eth0"                 # 发送的网卡
-my_ip = "192.168.76.200"       # 你要广播的 IP
-my_mac = "00:11:22:33:44:55"   # 你自己的 MAC 地址
+def build_gratuitous_arp(my_ip, my_mac):
+    eth = Ether(dst="ff:ff:ff:ff:ff:ff", src=my_mac)
+    # 构造 ARP reply（Gratuitous ARP）
+    arp = ARP(op=2,               # 2 = reply
+            hwsrc=my_mac,        # 本机 MAC
+            psrc=my_ip,          # 本机 IP
+            hwdst="ff:ff:ff:ff:ff:ff",  # 广播
+            pdst=my_ip)          # IP 发给自己（Gratuitous）
 
-# 构造以太网帧（广播）
-eth = Ether(dst="ff:ff:ff:ff:ff:ff", src=my_mac)
-
-# 构造 ARP reply（Gratuitous ARP）
-arp = ARP(op=2,               # 2 = reply
-          hwsrc=my_mac,        # 本机 MAC
-          psrc=my_ip,          # 本机 IP
-          hwdst="ff:ff:ff:ff:ff:ff",  # 广播
-          pdst=my_ip)          # IP 发给自己（Gratuitous）
-
-# 组合并发送
-pkt = eth / arp
-sendp(pkt, iface=iface, verbose=True)
+    return eth / arp
+    
+if __name__ == '__main__':
+    iface = "ens160"   
+    my_ip = "172.17.9.216" 
+    my_mac = "00:50:56:a4:22:1a"
+    pkt = build_gratuitous_arp(my_ip, my_mac)
+    sendp(pkt, iface=iface, verbose=True)
